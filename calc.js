678ba18d2fd66a973acf28a0e58278f0a54d2af9
Yzuhor <atti.refi@gmail.com>
2021-10-01 11:43:03 +0300

    first_commit

---
+++ calc.js
@@ -0,0 +1,15 @@
+let boxArr=document.querySelectorAll('.box')
+//feliratkozunk a click:
+boxArr.forEach(obj=>obj.addEventListener('click',myCalc))
+
+function myCalc(){
+    console.log(this.innerHTML)
+    let x=this.innerHTML
+    let boxR=document.querySelector('.boxResult')
+    if(x=='C')
+        boxR.innerHTML=''
+        else if(x=='=')
+        boxR.innerHTML=eval(boxR.innerHTML)
+        else
+        boxR.innerHTML+=x
+}
